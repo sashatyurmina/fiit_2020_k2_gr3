8d09b942b39abe8738f9edb88234176ad2ed3b05
sashatyurmina <43138508+sashatyurmina@users.noreply.github.com>
2020-05-14 22:54:15 +0300

    Update TMonom.cpp

--- Tyurmina/Polinom/Polinom/TMonom.cpp
+++ Tyurmina/Polinom/Polinom/TMonom.cpp
@@ -1,5 +1,6 @@
 #include "TMonom.h"
 #include <iostream>
+
 using namespace std;
 
 TNode<unsigned int, double>::TNode()
@@ -10,7 +11,6 @@ TNode<unsigned int, double>::TNode()
 }
 
 TNode<unsigned int, double>::TNode(const TNode<unsigned int, double>& n)
-
 {
 	degree = n.degree;
 	coeff = n.coeff;
@@ -49,12 +49,15 @@ bool TNode<unsigned int, double>::operator==(const TNode& monom) const
 TNode<unsigned int, double>* TNode<unsigned int, double>::operator*(const TNode<unsigned int, double>& monom)
 {
 	TNode<unsigned int, double>* result = new TNode<unsigned int, double>();
+
 	unsigned int deg_x1 = monom.degree / 100;
 	unsigned int deg_y1 = (monom.degree % 100) / 10;
 	unsigned int deg_z1 = monom.degree % 10;
+
 	unsigned int deg_x2 = this->degree / 100;
 	unsigned int deg_y2 = (this->degree % 100) / 10;
 	unsigned int deg_z2 = this->degree % 10;
+
 	if ((deg_x1 + deg_x2 > 9) || (deg_y1 + deg_y2 > 9) || (deg_z1 + deg_z2 > 9))
 		throw "Degree shouldn't be > 9";
 	result->degree = this->degree + monom.degree;
